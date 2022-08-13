096ed34303b7334b8154e649350b42d819154bc8
DELL <DELL@DESKTOP-9864HC3>
2022-08-14 00:25:59 +0200

    Add DbContext And Connection String

---
+++ Guestbook/Context/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Guestbook.Context
+{
+    public class DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionstring;
+
+
+        public DbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionstring = _configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionstring);
+    }
+}
